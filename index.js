f681383fc4fa96d350bbea43758a958895ba002f
jake-knox- <jakeknox11@hotmail.co.uk>
2023-05-26 14:08:16 +0100

    starter work on the stricter checks, working for the left-right / east-west tiles

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// v0.1 - 26-5-2023 Jake Knox
 
 let grid = 
 [
@@ -8,8 +9,21 @@ let grid =
     ["E1","E2","E3","E4","E5"],
 ];
 
+// used to make the strings from chars
 let str = "";
 let k = 0;
+
+// used for backtracking before check
+let m = 0;
+
+// will be used to stop double checks
+let foundWE3 = false;
+let foundWE5 = false;
+
+
+let foundNWEE = false;
+let foundNS = false;
+let foundNESW = false;
 
 for(let i = 0; i< 5; i++){
     // console.log(1);
@@ -18,10 +32,17 @@ for(let i = 0; i< 5; i++){
         console.log(2);
         console.log(grid[i][j]);
 
+        foundWE3 = false;
+        foundWE5 = false;
+
+        foundNWEE = false;
+        foundNS = false;
+        foundNESW = false;
+
         // West to East
         str = "";
-        let k = 0;
-        while(grid[i][j+k]){
+        k = 0;
+        while(j + k < 5){
             // console.log(3);
             str += grid[i][j+k];
             if(str.length > 2){
@@ -29,6 +50,68 @@ for(let i = 0; i< 5; i++){
             }
             k++;
         }
+        // start from 1 back
+        m = j-1
+        if(m > -1)
+        {
+            str = "";
+            k = 0;
+            while(m + k < 5){
+                // console.log(3);
+                str += grid[i][m+k];                
+                
+                console.log(`BW -> BE | ${str}`);                
+                                
+                k++;
+            }
+        }
+        m = j-2
+        if(m > -1)
+        {
+            str = "";
+            k = 0;
+            while(m + k < 5){
+                // console.log(3);
+                str += grid[i][m+k];
+                if(str.length == 10)
+                {
+                    console.log(`BW -> 5E | ${str}`);
+                }
+                k++;
+            }
+        }
+
+        // East to West
+        str = "";
+        k = 0;
+        while(j - k > -1){
+            str += grid[i][j-k];
+            if(str.length > 2){
+                console.log(`E- -> W- | ${str}`);
+            }
+            k++;
+        }
+        m = j+1
+        if(m < 5)
+        {
+            str = "";
+            k = 0;
+            while(m - k > -1){
+                // console.log(3);
+                str += grid[i][m-k];
+                if(str.length == 6 && foundWE3 == false)
+                {
+                    foundWE3 = true;
+                    console.log(`BE -> BW | ${str}`);
+                }
+                else if(str.length > 6)
+                {
+                    console.log(`BE -> BW | ${str}`);
+                }                   
+                k++;
+            }
+        }        
+
 
         // North-West to South-East
         str = "";
@@ -63,16 +146,7 @@ for(let i = 0; i< 5; i++){
             k++;
         }
 
-        // East to West
-        str = "";
-        k = 0;
-        while(j - k > -1){
-            str += grid[i][j-k];
-            if(str.length > 2){
-                console.log(`E- -> W- | ${str}`);
-            }
-            k++;
-        }
+       
 
         // South-East to North-West
         str = "";
